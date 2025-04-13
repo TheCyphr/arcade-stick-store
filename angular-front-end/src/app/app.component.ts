02115c6e05280d1a23e36664e04a28b9dfff504e
TheCyphr <conorlowry@live.ie>
2025-04-13 22:03:56 +0100

    Added header and footer angular components

--- angular-front-end/src/app/app.component.ts
+++ angular-front-end/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from "./core/layout/footer.component";
+import {HeaderComponent} from "./core/layout/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-front-end';
